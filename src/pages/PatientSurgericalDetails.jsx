2d67b5e020fe009581f6e153a77004a59da67e1b
Prabal Bhandary <prabalbhandary97@gmail.com>
2025-03-29 21:42:41 +0545

    navigate to the pages

--- src/pages/PatientSurgericalDetails.jsx
+++ src/pages/PatientSurgericalDetails.jsx
@@ -8,54 +8,137 @@ import skeleton from "../assets/skeleton.png";
 const SkeletonOverlay = ({ onPartClick }) => {
   const bodyParts = {
     upperLimb: [
-      { name: 'Clavicle', top: '20%', left: '45%', width: '10%', height: '5%', link: "/clavicle" },
-      { name: 'Scapula', top: '25%', left: '35%', width: '10%', height: '10%', link: "/scapula" },
-      { name: 'Humerus', top: '35%', left: '30%', width: '10%', height: '20%', link: "/humerus" },
-      { name: 'Elbow', top: '45%', left: '25%', width: '8%', height: '8%', link: "/elbow" },
-      { name: 'Radius & Ulna', top: '50%', left: '20%', width: '10%', height: '20%', link: "/radius_and_ulna" },
-      { name: 'Hand', top: '70%', left: '15%', width: '10%', height: '10%', link: "/hand" }
+      {
+        name: "Clavicle",
+        top: "20%",
+        left: "45%",
+        width: "10%",
+        height: "5%",
+        link: "/clavicle",
+      },
+      {
+        name: "Scapula",
+        top: "25%",
+        left: "35%",
+        width: "10%",
+        height: "10%",
+        link: "/scapula",
+      },
+      {
+        name: "Humerus",
+        top: "35%",
+        left: "30%",
+        width: "10%",
+        height: "20%",
+        link: "/humerus",
+      },
+      {
+        name: "Elbow",
+        top: "45%",
+        left: "25%",
+        width: "8%",
+        height: "8%",
+        link: "/elbow",
+      },
+      {
+        name: "Radius & Ulna",
+        top: "50%",
+        left: "20%",
+        width: "10%",
+        height: "20%",
+        link: "/radius_and_ulna",
+      },
+      {
+        name: "Hand",
+        top: "70%",
+        left: "15%",
+        width: "10%",
+        height: "10%",
+        link: "/hand",
+      },
     ],
     lowerLimb: [
-      { name: 'Pelvis', top: '50%', left: '42%', width: '16%', height: '10%', link: "/pelvis" },
-      { name: 'Acetabulum', top: '52%', left: '45%', width: '10%', height: '5%', link: "/acetabulum" },
-      { name: 'Femur', top: '60%', left: '40%', width: '10%', height: '20%', link: "/femur" },
-      { name: 'Tibia & Fibula', top: '75%', left: '40%', width: '10%', height: '20%', link: "/tibia_and_fibula" },
-      { name: 'Patella', top: '65%', left: '45%', width: '5%', height: '5%', link: "/patella" },
-      { name: 'Foot', top: '90%', left: '40%', width: '10%', height: '10%', link: "/foot" }
-    ]
+      {
+        name: "Pelvis",
+        top: "50%",
+        left: "42%",
+        width: "16%",
+        height: "10%",
+        link: "/pelvis",
+      },
+      {
+        name: "Acetabulum",
+        top: "52%",
+        left: "45%",
+        width: "10%",
+        height: "5%",
+        link: "/acetabulum",
+      },
+      {
+        name: "Femur",
+        top: "60%",
+        left: "40%",
+        width: "10%",
+        height: "20%",
+        link: "/femur",
+      },
+      {
+        name: "Tibia & Fibula",
+        top: "75%",
+        left: "40%",
+        width: "10%",
+        height: "20%",
+        link: "/tibia_and_fibula",
+      },
+      {
+        name: "Patella",
+        top: "65%",
+        left: "45%",
+        width: "5%",
+        height: "5%",
+        link: "/patella",
+      },
+      {
+        name: "Foot",
+        top: "90%",
+        left: "40%",
+        width: "10%",
+        height: "10%",
+        link: "/foot",
+      },
+    ],
   };
 
   const navigate = useNavigate();
 
   return (
     <div className="relative w-full max-w-md mx-auto">
-      <img 
-        src={skeleton} 
-        alt="skeleton" 
-        className="w-full h-auto opacity-50" 
-      />
+      <img src={skeleton} alt="skeleton" className="w-full h-auto opacity-50" />
       <div className="absolute inset-0">
-        {Object.entries(bodyParts).map(([limb, parts]) => (
+        {Object.entries(bodyParts).map(([limb, parts]) =>
           parts.map((part, index) => (
-            <button 
+            <button
               key={`${limb}-${index}`}
-              onClick={() => onPartClick(part.name)}
+              onClick={() => {
+                onPartClick(part.name);
+                navigate(part.link);
+              }}
               className="absolute hover:bg-blue-200/50 focus:outline-none"
               style={{
                 top: part.top,
                 left: part.left,
                 width: part.width,
                 height: part.height,
-                cursor: 'pointer'
+                cursor: "pointer",
               }}
               title={part.name}
             >
-              <span onClick={() => navigate(part.link)} className="text-xs text-white bg-blue-500/50 rounded px-1">
+              <span className="text-xs text-white bg-blue-500/50 rounded px-1">
                 {part.name}
               </span>
             </button>
           ))
-        ))}
+        )}
       </div>
     </div>
   );
@@ -154,9 +237,7 @@ const PatientSurgicalDetails = () => {
                   className="w-full border border-gray-300 rounded-md p-2"
                 >
                   <option value="normal">4 or less - Normal</option>
-                  <option value="laxity">
-                    5 or more - Ligamentous laxity
-                  </option>
+                  <option value="laxity">5 or more - Ligamentous laxity</option>
                 </select>
               </div>
 
@@ -190,7 +271,9 @@ const PatientSurgicalDetails = () => {
 
               {selectedBodyPart && (
                 <div>
-                  <p className="font-semibold text-gray-700">Selected Body Part</p>
+                  <p className="font-semibold text-gray-700">
+                    Selected Body Part
+                  </p>
                   <p className="text-xl text-gray-800">{selectedBodyPart}</p>
                 </div>
               )}
