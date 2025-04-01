edbb27baaf5b99cef28662750b56d639a2559580
Prabal Bhandary <prabalbhandary97@gmail.com>
2025-04-01 14:13:23 +0545

    changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -133,10 +133,10 @@ const Register = () => {
                 className="w-full mt-2 p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
               <span
-                onClick={() => setShowPassword(!showPassword)}
+                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
                 className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
               >
-                {showPassword ? (
+                {showConfirmPassword ? (
                   <FaEyeSlash className="text-gray-600 text-xl" />
                 ) : (
                   <FaEye className="text-gray-600 text-xl" />
