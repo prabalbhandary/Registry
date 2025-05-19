a40e1a509387f7b7041d3ea77a26a0efeafb9835
Prabal Bhandary <prabalbhandary97@gmail.com>
2025-05-19 13:18:08 +0545

    Refactor Users component to fetch and display user data with loading and error handling

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,10 +1,73 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { URL } from "../components/URL";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 
 const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get(`${URL}/user`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        })
+        if (res.status === 200) {
+          setUsers(res.data.data);
+        } else {
+          toast.error("Failed to fetch users");
+        }
+      } catch (error) {
+        setError(error);
+        console.error("Error fetching users:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, [])
+
   return (
     <>
       <title>Users - Trauma Registry</title>
-      Users
+      <div className="bg-white text-black p-4 flex justify-between items-center">
+        <h2 className="text-2xl font-bold">Users</h2>
+      </div>
+      <hr />
+      <div className="overflow-x-auto">
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>Error fetching users: {error.message}</p>
+        ) : (
+          <table className="min-w-full bg-white">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b">ID</th>
+                <th className="py-2 px-4 border-b">Username</th>
+                <th className="py-2 px-4 border-b">Email</th>
+                <th className="py-2 px-4 border-b">Role</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id}>
+                  <td className="py-2 px-4 border-b">{user.id}</td>
+                  <td className="py-2 px-4 border-b">{user.name}</td>
+                  <td className="py-2 px-4 border-b">{user.email}</td>
+                  <td className="py-2 px-4 border-b">{user.is_admin ? "Admin" : "User"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </>
   );
 };
