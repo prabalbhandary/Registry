57dc7506cc2a3728fd18f83548540734682de409
Prabal Bhandary <109888062+prabalbhandary@users.noreply.github.com>
2025-12-22 22:20:53 +0545

    Fix NavLink rendering based on user role

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -217,7 +217,8 @@ const Sidebar = () => {
             <span className="font-medium">Dashboard</span>
           </NavLink>
 
-          {role && <NavLink
+          {role && 
+            (<NavLink
             to="/all-patients"
             onClick={() => setMenuOpen(false)}
             className={({ isActive }) =>
@@ -228,7 +229,7 @@ const Sidebar = () => {
           >
             <HiUsers className="text-xl" />
             <span className="font-medium">All Patients</span>
-          </NavLink>}
+          </NavLink>)}
 
           <NavLink
             to="/patients/follow-up"
