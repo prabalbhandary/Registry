d58a02111054620ad2f552a3e4fa1f2c4155597e
Prabal Bhandary <bhandaryprabal837@gmail.com>
2026-03-12 12:37:00 +0545

    svg to react-icon

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import axios from "axios";
 import { URL } from "../components/URL";
-import { FaEnvelope, FaArrowLeft, FaLock } from "react-icons/fa";
+import { FaEnvelope, FaArrowLeft, FaLock, FaKey } from "react-icons/fa";
 
 const ForgetPassword = () => {
   const [email, setEmail] = useState("");
@@ -138,19 +138,7 @@ const ForgetPassword = () => {
               {/* Form Header */}
               <div className="text-center mb-8">
                 <div className="inline-block p-4 bg-gradient-to-br from-orange-500 to-red-600 rounded-2xl mb-4">
-                  <svg
-                    className="w-8 h-8 text-white"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
-                    />
-                  </svg>
+                  <FaKey className="w-8 h-8 text-white" />
                 </div>
                 <h2 className="text-3xl font-bold text-slate-800 mb-2">
                   Reset Password
