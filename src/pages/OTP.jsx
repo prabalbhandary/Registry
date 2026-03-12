88700ae7e019d305ff5499795181919883c05320
Prabal Bhandary <bhandaryprabal837@gmail.com>
2026-03-12 13:18:57 +0545

    svg to react-icon

--- src/pages/OTP.jsx
+++ src/pages/OTP.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
 import { URL } from "../components/URL";
 import axios from "axios";
-import { FaShieldAlt, FaArrowLeft, FaEnvelope, FaClock } from "react-icons/fa";
+import { FaShieldAlt, FaArrowLeft, FaEnvelope, FaClock, FaInfoCircle } from "react-icons/fa";
 import { LiaSpinnerSolid } from "react-icons/lia";
 
 const OTP = () => {
@@ -272,17 +272,7 @@ const OTP = () => {
             {/* Additional Info */}
             <div className="mt-8 p-4 bg-white/60 backdrop-blur-sm rounded-2xl border border-slate-200">
               <div className="flex items-start gap-3">
-                <svg
-                  className="w-5 h-5 text-blue-500 flex-shrink-0 mt-0.5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <FaInfoCircle className="w-5 h-5 text-blue-500 shrink-0 mt-0.5" />
                 <p className="text-sm text-slate-600">
                   For security reasons, this code will expire in 10 minutes. If
                   you need a new code, use the "Resend OTP Code" button above.
