4f994d9ff10c4263dce8b4e69aeaa50f882c3475
Prabal Bhandary <bhandaryprabal837@gmail.com>
2026-03-12 12:29:02 +0545

    updated

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -45,6 +45,7 @@ const ForgetPassword = () => {
             <div className="text-center lg:text-left">
               <div className="flex items-center justify-center lg:justify-start gap-4 mb-6">
                 <img
+                  onClick={() => navigate("/")}
                   src={Logo}
                   alt="Nepal Orthopedic Association"
                   className="w-20 h-20 lg:w-24 lg:h-24 object-contain bg-white/10 backdrop-blur-sm rounded-2xl p-2 border border-white/20"
